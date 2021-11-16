2cc73600e5d802df976c357f59c7b809f9f83830
Nattaphat Laoharawee <nattaphat.la@gmail.com>
2021-11-16 14:33:32 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,78 +1,47 @@
-import React from 'react';
-import {
-  Form,
-  Select,
-  InputNumber,
-  DatePicker,
-  Switch,
-  Slider,
-  Button,
-  Rate,
-  Typography,
-  Space,
-  Divider,
-} from 'antd';
-import './App.less';
+import { Button, Layout, Result, Skeleton } from 'antd';
+import './App.less'
+import React, { useEffect } from 'react';
+import { useLiff } from 'react-liff';
 
-const { Option } = Select;
-const { Title } = Typography;
+const App = () => {
+  const { error, liff, isLoggedIn, ready } = useLiff();
 
-const App = () => (
-  <>
-    <section style={{ textAlign: 'center', marginTop: 48, marginBottom: 40 }}>
-      <Space align="start">
-        <img
-          style={{width: 40, height: 40 }}
-          src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
-          alt="Ant Design"
-        />
-        <Title level={2} style={{ marginBottom: 0 }}>
-          Ant Design
-        </Title>
-      </Space>
-    </section>
-    <Divider style={{ marginBottom: 60 }}>Form</Divider>
-    <Form labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
-      <Form.Item label="数字输入框">
-        <InputNumber min={1} max={10} defaultValue={3} />
-        <span className="ant-form-text"> 台机器</span>
-        <a href="https://ant.design">链接文字</a>
-      </Form.Item>
-      <Form.Item label="开关">
-        <Switch defaultChecked />
-      </Form.Item>
-      <Form.Item label="滑动输入条">
-        <Slider defaultValue={70} />
-      </Form.Item>
-      <Form.Item label="选择器">
-        <Select defaultValue="lucy" style={{ width: 192 }}>
-          <Option value="jack">jack</Option>
-          <Option value="lucy">lucy</Option>
-          <Option value="disabled" disabled>disabled</Option>
-          <Option value="yiminghe">yiminghe</Option>
-        </Select>
-      </Form.Item>
-      <Form.Item label="日期选择框">
-        <DatePicker />
-      </Form.Item>
-      <Form.Item label="日期范围选择框">
-        <DatePicker.RangePicker />
-      </Form.Item>
-      <Form.Item label="评分">
-        <Rate defaultValue={5} />
-      </Form.Item>
-      <Form.Item wrapperCol={{ span: 8, offset: 8 }}>
-        <Space>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-          <Button>
-            Cancel
-          </Button>
-        </Space>
-      </Form.Item>
-    </Form>
-  </>
-);
+  useEffect(() => {
+    if (!isLoggedIn) {
+      liff.login()
+    }
+  }, [liff, isLoggedIn]);
+  if (!ready) {
+    return <Skeleton />
+  }
+  if (error) {
+    console.error(error)
+    return <Result status="500"/>
+  }
+  const handleScanCode = async () => {
+    try {
+    const result = await liff.scanCodeV2();
+    console.log(result)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+  return (
+    <React.Fragment>
+      <Layout style={{minHeight: '100vh'}}>
+        <Layout.Header>
+          Header
+        </Layout.Header>
+        <Layout.Content>
+          Body
+          <Button onClick={handleScanCode}>Scan Code</Button>
+        </Layout.Content>
+        <Layout.Footer>
+          Footer
+        </Layout.Footer>
+      </Layout>
+    </React.Fragment>
+  );
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { LiffProvider } from 'react-liff';
+import liff from '@line/liff'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const liffId = process.env.REACT_APP_LINE_LIFF_ID;
+const liffConfig = {
+  liffId
+}
+liff.init(liffConfig).then(() => {
+  ReactDOM.render(
+    <LiffProvider stubEnabled={{
+      ...liff,
+      login: () => {
+        return liff.login({
+          redirectUri: window.location.origin
+        })
+      }
+    }}>
+      <App />
+    </LiffProvider>,
+    document.getElementById('root')
+  );
+})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
